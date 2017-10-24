f0cb3ed2797cad86e890d2c93fd4c3dd3d63af0a
Mikhail Gusarov <dottedmag@dottedmag.net>
2017-10-24 20:33:33 +0200

    Add Clojure tools config

---
+++ .lein/profiles.clj
@@ -0,0 +1,6 @@
+{:auth {:repository-auth {#"clojars" {:username "dottedmag"
+                                      :password "x^ZU\\{f,Gkc',:3x:8D<"}}}
+#_ :signing #_ {:gpg-key "56008008"}
+ :user {:plugins [[lein-try "0.4.3"]
+                  [lein-ancient "0.6.10"]]}
+ :repl {:history-file [(user/leiningen-home) "repl-history"]}}
