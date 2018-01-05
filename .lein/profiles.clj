6848d3848c8c9364896c3848015d6fb6acbaaa97
Mikhail Gusarov <dottedmag@dottedmag.net>
2018-01-05 20:50:38 +0100

    More useful Leiningen plugins by default

--- .lein/profiles.clj
+++ .lein/profiles.clj
@@ -2,5 +2,7 @@
                                       :password "x^ZU\\{f,Gkc',:3x:8D<"}}}
 #_ :signing #_ {:gpg-key "56008008"}
  :user {:plugins [[lein-try "0.4.3"]
-                  [lein-ancient "0.6.10"]]}
+                  [lein-ancient "0.6.10"]
+                  [lein-kibit "0.1.5"]
+                  [jonase/eastwood "0.2.5"]]}
  :repl {:history-file [(user/leiningen-home) "repl-history"]}}
